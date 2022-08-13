1fe7ac946334a894554fa57036153c6d4560dcfc
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-08-13 21:39:09 +0200

    pizza toppings + prices

---
+++ script.py
@@ -0,0 +1,8 @@
+toppings = ['pepperoni', 'pineapple', 'cheese',
+            'sausage', 'olives', 'anchovies', 'mushrooms']
+prices = [2, 6, 1, 3, 2, 7, 2]
+
+num_two_dollar_slices = prices.count(2)
+num_pizzas = len(toppings)
+
+print('We sell', num_pizzas, 'different kinds of pizza!')
