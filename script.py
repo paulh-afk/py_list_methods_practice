ec7d22d3097ea9f9f365a7a7e4d0cbcf13f8cfb8
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-08-13 22:10:04 +0200

    store 3 lowest cost pizza

--- script.py
+++ script.py
@@ -11,11 +11,12 @@ pizza_and_prices = [[2, 'pepperoni'], [6, 'pineapple'], [1, 'cheese'], [
     3, 'sausage'], [2, 'olives'], [7, 'anchovies'], [2, 'mushrooms']]
 
 pizza_and_prices.sort()
-print(pizza_and_prices)
 
 cheapest_pizza = pizza_and_prices[0][1]
 priciest_pizza = pizza_and_prices[-1][1]
 
 pizza_and_prices.pop(-1)
 pizza_and_prices.insert(4, [2.5, 'peppers'])
-print(pizza_and_prices)
+
+three_cheapest = pizza_and_prices[0:3]
+print(three_cheapest)
