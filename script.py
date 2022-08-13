7d8036fc92a239e539fb319dae59358d4ccc6066
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-08-13 21:55:03 +0200

    pizza_and_prices list

--- script.py
+++ script.py
@@ -6,3 +6,6 @@ num_two_dollar_slices = prices.count(2)
 num_pizzas = len(toppings)
 
 print('We sell', num_pizzas, 'different kinds of pizza!')
+
+pizza_and_prices = [[2, 'pepperoni'], [6, 'pineapple'], [1, 'cheese'], [
+    3, 'sausage'], [2, 'olives'], [7, 'anchovies'], [2, 'mushrooms']]
